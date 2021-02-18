2e7f6d82a98fd79b804bd444d724b4fc68b53206
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-02-18 21:26:02 +0200

    38. Finish Game Logic

--- guess-the-number-game/console/src/main/java/com/gmail/ryitlearning/console/ConsoleNumberGuess.java
+++ guess-the-number-game/console/src/main/java/com/gmail/ryitlearning/console/ConsoleNumberGuess.java
@@ -1,10 +1,15 @@
 package com.gmail.ryitlearning.console;
 
+import com.gmail.ryitlearning.Game;
+import com.gmail.ryitlearning.MessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 //public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
@@ -16,8 +21,40 @@ public class ConsoleNumberGuess {
 //    public void start(ContextRefreshedEvent contextRefreshedEvent) {
 //        log.info("start() --> Container ready to use");
 //    }
+
+    // == fields ==
+    @Autowired
+    private Game game;
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    // == Events ==
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
         log.info("start() --> Container ready to use");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost()){
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+
+                String playAgainString = scanner.nextLine().trim();
+                if (!playAgainString.equalsIgnoreCase("y")){
+                    break;
+                }
+                game.reset();
+            }
+        }
     }
 }
