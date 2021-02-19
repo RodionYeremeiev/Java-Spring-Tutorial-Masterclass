63362ab27db94a13dea28f278c51af59e2f6ae30
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-02-19 18:40:31 +0200

    40. Creating Custom Annotations 41. Using Properties

--- guess-the-number-game/core/src/main/java/com/gmail/ryitlearning/NumberGeneratorImpl.java
+++ guess-the-number-game/core/src/main/java/com/gmail/ryitlearning/NumberGeneratorImpl.java
@@ -11,6 +11,7 @@ public class NumberGeneratorImpl implements NumberGenerator {
     // == fields ==
     private final Random random = new Random();
     @Autowired
+    @MaxNumber
     private int maxNumber;
 
     // == public methods ==
--- guess-the-number-game/core/src/main/java/com/gmail/ryitlearning/config/GameConfig.java
+++ guess-the-number-game/core/src/main/java/com/gmail/ryitlearning/config/GameConfig.java
@@ -1,21 +1,31 @@
 package com.gmail.ryitlearning.config;
 
+import com.gmail.ryitlearning.GuessCount;
+import com.gmail.ryitlearning.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
-    private int guessCount = 8;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
+    @MaxNumber
     public int maxNumber(){
         return maxNumber;
     }
     @Bean
+    @GuessCount
     public int guessCount(){
         return guessCount;
     }
